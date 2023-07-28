41b5ceaee71f796e64d1131987677a339658e311
lukatelia <138887175+lukatelia@users.noreply.github.com>
2023-07-28 22:58:21 +0400

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,37 @@
+#pirveli davaleba 
+davaleba_1= int(input("100/5 = "))
+
+davaleba_2= int (input("100 + 113 - 20 +5 = "))
+
+davaleba_3 = int (input ("150 * 14= "))
+
+davaleba_4 = int ( input ( "420 - 20 + 100 = "))
+
+pasuxebi= (davaleba_1==20) and (davaleba_2==198) and (davaleba_3==2100) and (davaleba_4==500)
+
+print(pasuxebi)
+
+
+#meore davaleba 
+task_1 = int(input (" 20 * 8 = "))
+
+task_2 = int(input (" 120 + 20 + 20 = " ))
+
+task_3 = int(input ( " 50 * 5 - 100 = "))
+
+task_4 = int ( input ( " 130 + 70 - 20 = "))
+
+answers = (task_1 == 160 ) or (task_2 == 160) or (task_3 == 150) or (task_4 == 180)
+
+print (answers) 
+
+#mesame davaleba
+task_5= str( input( " 20 + 5 * 10 = "))
+
+task_6 = str ( input (" 10 + 5 = "))
+
+answer= (task_5 == 70) and (task_6 == 15 )
+
+conclusion = (task_5 > task_6 ) 
+
+print ( conclusion )
